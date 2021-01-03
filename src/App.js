a45b926564465adbb99af0b3f5f2f3d913a83f9f
Dice-K39 <daisukehashimoto39@gmail.com>
2021-01-02 22:03:45 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import React, { Component } from 'react';
 
 class App extends Component
 {
+    state =
+    {
+        peopleInSpace: []
+    }
+
     componentDidMount()
     {
         const url = "http://api.open-notify.org/astros.json";
 
         fetch(url)
             .then(resp => resp.json())
-            .then(data => console.log(data));
+            .then(({people}) => this.setState({peopleInSpace: people}));
     }
 
     render()
     {
-        return 0;
+        return (
+            <div>
+                {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        );
     }
 }
 
